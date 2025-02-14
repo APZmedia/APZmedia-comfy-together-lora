1d555d4c565ab5851df70398cd5e1a28a6b24676
Pablo APZmedia <me@apzmedia.com>
2025-02-14 19:01:00 +0100

    Less stuff

--- examples/__init__.py
+++
@@ -1,9 +0,0 @@
-from .nodes.together_image_generator import TogetherImageGenerator
-
-NODE_CLASS_MAPPINGS = {
-    "TogetherImageGenerator": TogetherImageGenerator,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "TogetherImageGenerator": "Together Image Generator with LoRA",
-}
